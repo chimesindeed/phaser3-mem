342d368086f085c931e91684cde8e69130e7a193
DevD <gratitudeadore@fastmail.com>
2021-11-28 15:08:32 -0500

    Added walking animations for each direction

--- src/scenes/Preloader.js
+++ src/scenes/Preloader.js
@@ -36,6 +36,33 @@ export default class Preloader extends Phaser.Scene {
 			frames: [{ key: 'sokoban', frame: 78 }]
 		})
 
+		this.anims.create({
+			key: 'down-walk',
+			frames: this.anims.generateFrameNumbers('sokoban', { start: 52, end: 54 }),
+			frameRate: 10,
+			repeat: -1
+		})
+
+		this.anims.create({
+			key: 'up-walk',
+			frames: this.anims.generateFrameNumbers('sokoban', { start: 55, end: 57 }),
+			frameRate: 10,
+			repeat: -1
+		})
+
+		this.anims.create({
+			key: 'left-walk',
+			frames: this.anims.generateFrameNumbers('sokoban', { start: 81, end: 83 }),
+			frameRate: 10,
+			repeat: -1
+		})
+
+		this.anims.create({
+			key: 'right-walk',
+			frames: this.anims.generateFrameNumbers('sokoban', { start: 78, end: 80 }),
+			frameRate: 10,
+			repeat: -1
+		})
 		this.scene.start('game')
 	}
 
